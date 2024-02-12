63a246aa0cfc5078498bd313ec0f2d32901e2e8b
bpasanek <bmp7e@virginia.edu>
2024-02-11 21:34:55 -0500

    Final Commit
    Sharing HW with class.

---
+++ HW4-Pasanek/scripts/DeathStar.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _draw():
+	draw_circle(Vector2(50,50),30,Color.GRAY)
---
+++ HW4-Pasanek/scripts/GodotInvader.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
+@export var speed = 100
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	path_follow.progress += speed*delta
+	
+# I used this tutorial as a model: https://www.youtube.com/watch?v=tHrT4KoDZ_Y
